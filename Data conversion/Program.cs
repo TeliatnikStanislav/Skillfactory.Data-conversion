a16a63de46829165765b4ca56d80f6ffc12a06f7
Stanislav <slava.telyatnik@mail.ru>
2023-07-13 09:30:22 +0200

    Добавьте файлы проекта.

---
+++ Data conversion/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://akausing System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter your age: ");
+        byte age = (byte)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        Console.ReadKey();
+    }
+}
