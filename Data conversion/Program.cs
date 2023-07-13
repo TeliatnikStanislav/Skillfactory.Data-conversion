108e4dc1adb20ae94ce8a3da1f259bd4096e1869
Stanislav <slava.telyatnik@mail.ru>
2023-07-13 09:30:57 +0200

    Checked

--- Data conversion/Program.cs
+++ Data conversion/Program.cs
@@ -7,7 +7,7 @@ class MainClass
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
         Console.Write("Enter your age: ");
-        byte age = (byte)int.Parse(Console.ReadLine());
+        byte age = checked((byte)int.Parse(Console.ReadLine()));
         Console.WriteLine("Your name is {0} and age is {1} ", name, age);
         Console.ReadKey();
     }
