19eba31d5b81a970a0e47262131910ef8b5f855a
Stanislav <slava.telyatnik@mail.ru>
2023-07-13 10:32:14 +0200

    3.7.4

--- Data conversion/Program.cs
+++ Data conversion/Program.cs
@@ -1,14 +1,30 @@
-﻿// See https://akausing System;
+﻿
 
 class MainClass
 {
     public static void Main(string[] args)
     {
+        Week favday;
         Console.Write("Enter your name: ");
         string name = Console.ReadLine();
         Console.Write("Enter your age: ");
-        byte age = checked((byte)int.Parse(Console.ReadLine()));
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Your name is {name} and age is {age}");
+        Console.WriteLine("What is your favorute day?");
+        Week MyFavDay = (Week) byte.Parse(Console.ReadLine());
+        Console.WriteLine(MyFavDay);
         Console.ReadKey();
     }
+}
+
+enum Week : byte
+{
+    Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
+
 }
